0618f13e2fd799cc78b7098d7bc77c4a2a04f9ec
Anthony Garcia <anthony.garcia-labiad@live.fr>
2013-04-03 22:30:04 +0200

    Add XLinq.cs

---
+++ Kifu/Utils/XLinq.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Kifu.Utils
+{
+    public static class XLinq
+    {
+        public static void Each<T>(this IEnumerable<T> enumberable, Action<T> action)
+        {
+            foreach (var item in enumberable)
+            {
+                action(item);
+            }
+        }
+
+        public static void Times(this int n, Action<int> action)
+        {
+            for (int i = 0; i < n; ++i)
+            {
+                action(i);
+            }
+        }
+    }
+}
